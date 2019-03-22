6f25338df5360abea716f468eb6beacfe22a2391
hyb <76788424@qq.com>
2019-03-22 16:14:40 +0800

    modify ini.go add some annotation

--- ini.go
+++ ini.go
@@ -1,8 +1,9 @@
+// Package toolbox 封装了一个 .ini 格式文件读取器
 package ini
 
 import (
-	"os"
 	"bufio"
+	"os"
 	"strings"
 )
 
@@ -12,10 +13,11 @@ type Section M
 type Conf map[string]Section
 
 var (
-	conf Conf = make(Conf)	// 初始化 conf
+	conf           Conf   = make(Conf) // 初始化 conf
 	currentSection string = "default"
 )
 
+// Read 从 .ini 文件中读取配置信息
 func Read(filename string) (Conf, error) {
 	file, err := os.Open(filename)
 	if err != nil {
@@ -23,34 +25,35 @@ func Read(filename string) (Conf, error) {
 	}
 	defer file.Close()
 
-	conf[currentSection] = make(Section)	// 初始化默认 section，conf['default']
+	conf[currentSection] = make(Section) // 初始化默认 section，conf['default']
 
-	scanner := bufio.NewScanner(file)	// bufio.Scanner 实现逐行读取
+	scanner := bufio.NewScanner(file) // bufio.Scanner 实现逐行读取
 	for {
-		if ok := scanner.Scan(); !ok {	// 逐行扫描，文件尾或遇到错误会返回 false
-			if err := scanner.Err(); err != nil {	// 返回第一个非 io.eof 的错误，如果是 io.eof，则返回的是 nil，如果有错误打印错误，并 exit
+		if ok := scanner.Scan(); !ok { // 逐行扫描，文件尾或遇到错误会返回 false
+			if err := scanner.Err(); err != nil { // 返回第一个非 io.eof 的错误，如果是 io.eof，则返回的是 nil，如果有错误打印错误，并 exit
 				return conf, err
 			}
-			break	// 执行到这说明文件读取完毕
-		}	
-		parse(scanner.Text())	// 返回最近一次 Scan 调用生成的文本
+			break // 执行到这说明文件读取完毕
+		}
+		parse(scanner.Text()) // 返回最近一次 Scan 调用生成的文本
 	}
 	return conf, nil
 }
 
+// 解析 .ini 文件行
 func parse(line string) {
-	if isNodeAnnotation(line) { 
-		return 
+	if isNodeAnnotation(line) {
+		return
 	}
 	if ok, section := isNodeSection(line); ok {
 		currentSection = section
 		if _, isExists := conf[currentSection]; !isExists {
-			conf[currentSection] = make(Section)	// 如果该 section 不存在，则初始化
+			conf[currentSection] = make(Section) // 如果该 section 不存在，则初始化
 		}
 		return
 	}
 	if ok, item := isNodeItem(line); ok {
-		for k,v := range item {
+		for k, v := range item {
 			conf[currentSection][k] = v
 		}
 	}
@@ -65,7 +68,7 @@ func isNodeAnnotation(line string) bool {
 
 func isNodeSection(line string) (bool, string) {
 	if strings.HasPrefix(line, "[") {
-		return true, strings.Trim(line, "[] ")	// 可以同时去除两端的"["、"]"、" "
+		return true, strings.Trim(line, "[] ") // 可以同时去除两端的"["、"]"、" "
 	}
 	return false, ""
 }
@@ -73,7 +76,7 @@ func isNodeSection(line string) (bool, string) {
 func isNodeItem(line string) (bool, M) {
 	item := strings.SplitN(line, "=", 2)
 	if len(item) == 2 {
-		return true, M{ strings.TrimSpace(item[0]) : strings.TrimSpace(item[1]) }
+		return true, M{strings.TrimSpace(item[0]): strings.TrimSpace(item[1])}
 	}
-	return false, nil	
+	return false, nil
 }
